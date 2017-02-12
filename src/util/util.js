46905b39b91af89020903254229614a7ffb84a85
lordbaby <lordbaby@163.com>
2017-02-12 23:04:23 +0800

    changepic

--- src/util/util.js
+++
@@ -1,4 +0,0 @@
-//获取区间内的一个随机值
-export const getRangeRandom= (low,high) => Math.floor(Math.random()*(high-low)+low);
-//随机生成一个0-30度正负角度
-export const get30DegRandom= ()=>Math.floor(Math.random()*60 -30);
